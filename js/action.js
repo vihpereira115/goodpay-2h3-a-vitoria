59a9a3b0ba6c920669d407e67352f740a8fd93b8
vihpereira115 <vihpereira115@gmail.com>
2026-04-14 20:52:03 -0300

    04/14 aula sobre nos

--- js/action.js
+++ js/action.js
@@ -32,3 +32,18 @@ function funcaoAccordeon(id) {
       x.className = x.className.replace(" w3-show", "");
     }
   }
+  document.getElementsByClassName("tablink")[0].click();
+
+function openAboutUs(evt, pageName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("page");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].classList.remove("w3-light-grey");
+  }
+  document.getElementById(pageName).style.display = "block";
+  evt.currentTarget.classList.add("w3-light-grey");
+}
